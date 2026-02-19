307e646cf0bdc4c194e61c0b719ea49a53509c71
Baz Rahimi <bazrahimi@hotmail.com>
2026-02-20 10:09:10 +1100

    add

---
+++ my-app/app/_lib/content/cta.ts
@@ -0,0 +1,12 @@
+export const cta = {
+  freeQuote: {
+    label: "Free Quote",
+    labelFarsi: "قیمت‌دهی مجانی",
+  },
+  freeConsultation: {
+    label: "Free Consultation",
+    labelFarsi: "مشاوره رایگان",
+  },
+};
+
+export type CtaKey = keyof typeof cta;
