acc70454836446a6447275cbc7bb7f2d2cf41dc2
Baz Rahimi <bazrahimi@hotmail.com>
2026-02-23 10:44:26 +1100

    add

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,24 +1,4 @@
-// // app/page.tsx
 
-// import { SERVICES } from "@/app/_lib/org/category/services";
-// import ServiceLeavesCarousel from "@/app/_ui/carousels/ServiceLeavesCarousel";
-
-// export default function HomePage() {
-//   return (
-//     <div className="space-y-12 p-6">
-//       {Object.entries(SERVICES).map(([key, service]) => (
-//         <ServiceLeavesCarousel
-//           key={key}
-//           slug={service.slug}
-//           subcategories={service.subcategories}
-//           heading={service.label}  // English
-
-//         />
-//       ))}
-
-//     </div>
-//   );
-// }
 import { SERVICES } from "@/app/_lib/org/category/services";
 import { Suspense } from "react";
 import { ORG_PROFILE as op } from "./_lib/org/profile";
