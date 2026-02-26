612f3cd158f56235288ec0e0d7a4d1ca14411f0f
Baz Rahimi <bazrahimi@hotmail.com>
2026-02-27 10:57:33 +1100

    add

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,5 +1,5 @@
 // app/layout.tsx
-import { ROOT_SEO, absoluteUrl } from "@/app/_lib/rootSeo";
+
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -13,56 +13,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const viewport: Viewport = {
-  themeColor: ROOT_SEO.themeColor,
-};
 
-export const metadata: Metadata = {
-  metadataBase: new URL(ROOT_SEO.baseUrl),
-
-  title: {
-    default: ROOT_SEO.title,
-    template: `%s | ${ROOT_SEO.siteName}`,
-  },
-
-  description: ROOT_SEO.description,
-
-  // Root-level canonical = homepage
-  alternates: {
-    canonical: ROOT_SEO.baseUrl,
-    languages: {
-      "en-AU": ROOT_SEO.baseUrl,
-      "fa-IR": `${ROOT_SEO.baseUrl}/fa`,
-    },
-  },
-
-  manifest: ROOT_SEO.manifestPath,
-  icons: ROOT_SEO.icons,
-  robots: ROOT_SEO.robots,
-
-  openGraph: {
-    type: "website",
-    siteName: ROOT_SEO.siteName,
-    url: ROOT_SEO.baseUrl,
-    title: ROOT_SEO.title,
-    description: ROOT_SEO.description,
-    images: [
-      {
-        url: absoluteUrl(ROOT_SEO.ogImagePath),
-        width: 1200,
-        height: 630,
-        alt: ROOT_SEO.siteName,
-      },
-    ],
-  },
-
-  twitter: {
-    card: "summary_large_image",
-    title: ROOT_SEO.title,
-    description: ROOT_SEO.description,
-    images: [absoluteUrl(ROOT_SEO.ogImagePath)],
-  },
-};
 
 export default function RootLayout({
   children,
