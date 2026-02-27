5c3105b0ccbb69f47201fb0564dfef599e25bb35
Baz Rahimi <bazrahimi@hotmail.com>
2026-02-27 15:59:38 +1100

    add

--- my-app/app/_lib/org/font.ts
+++ my-app/app/_lib/org/font.ts
@@ -1,13 +1,9 @@
-import { Inter, Playfair_Display, Roboto } from "next/font/google";
+import { Inter, Playfair_Display } from "next/font/google";
 export const uiFond = Inter({ subsets: ["latin"] });
 export const headingFont = Playfair_Display({
   weight: ["400", "700"],
   subsets: ["latin"],
 });
 
-// TODO: change ROBOTo as well
-export const bodyFont = Roboto({
-  weight: ["400", "500", "700"],
-  subsets: ["latin"],
-  display: "swap",
-});
+
+export const bodyFont = uiFond;
